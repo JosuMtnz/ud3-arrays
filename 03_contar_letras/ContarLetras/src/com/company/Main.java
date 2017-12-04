25d774026dc28f3807869455fff2da9566a81bda
Unknown <josu.mtnz@gmail.com>
2017-12-04 21:12:52 +0100

    Ejercicio 03 terminado.

---
+++ 03_contar_letras/ContarLetras/src/com/company/Main.java
@@ -0,0 +1,44 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("Escribe un texto: ");
+        String texto = br.readLine().toUpperCase();
+
+        String abc = "ABCDEFGHIJKMNÑOPQRSTUVWXYZ";
+
+        int[] recuento = new int[abc.length()];
+        contarletras(texto, abc, recuento);
+        visualizarRecuento(abc, recuento);
+        
+    }
+
+    private static void visualizarRecuento(String abc, int[] recuento) {
+        for (int i = 0; i < recuento.length; i++) {
+            if ( recuento[i] > 0 ){
+                // System.out.format("d% : d%",abc.charAt(i), recuento[i]);
+                System.out.println(abc.charAt(i)+": "+recuento[i]);
+            }
+        }
+    }
+
+    private static void contarletras(String texto, String abc, int[] recuento) {
+        for (int i = 0; i < texto.length(); i++) {
+            char letra = texto.charAt(i);
+            for (int j = 0; j < abc.length(); j++) {
+                if (abc.charAt(j) == letra ){
+                    recuento[j] += 1;
+                }
+            }
+        }
+    }
+}
